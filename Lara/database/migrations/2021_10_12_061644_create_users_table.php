d699ecd981238d6c6829484d1b9b8ea6be360cfc
Alicia Hsiang <31757903+foxlovebear@users.noreply.github.com>
2021-10-22 11:24:20 +0800

    Add files via upload

---
+++ Lara/database/migrations/2021_10_12_061644_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('email', 100);
+            $table->string('password', 25);
+            $table->string('name', 25);
+            $table->string('address', 100);
+            $table->string('phone', 10);
+            $table->string('photo', 150);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
