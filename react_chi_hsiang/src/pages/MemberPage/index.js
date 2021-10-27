458f9e22fcaada5b2089f7ad30b884000106213f
Alicia_Hsiang <b01101049@gmail.com>
2021-10-27 20:36:12 +0800

    Login 和 Register Page JSX畫面

--- react_chi_hsiang/src/pages/MemberPage/index.js
+++ react_chi_hsiang/src/pages/MemberPage/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Link} from 'react-router-dom';
+
+// 使用react-bootstrap 和CSS
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {Container,Row,Col,Stack,
+ Form,
+ FormControl,
+ Button } from 'react-bootstrap';
+//使用
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// -----------------------Import End----------------
+
+
+function FMemberPage(){
+  return(
+  <Container>
+    <Row >
+      <Col>
+        <Stack gap={3}>
+          <div className="bg-light border">First item</div>
+          <div className="bg-light border">Second item</div>
+          <div className="bg-light border">Third item</div>
+        </Stack>
+      </Col>
+    </Row>
+  </Container>
+  )
+}
+
+export default FMemberPage;
